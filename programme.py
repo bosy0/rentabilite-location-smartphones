6c8257cc1e7348252d88ba01f50759c76a202185
Nathan BOSY <104134380+bosy0@users.noreply.github.com>
2023-04-25 16:24:29 +0200

    MAJ PRIX + BUG TXT

--- programme.py
+++ programme.py
@@ -7,29 +7,38 @@ texte=str("####################################################"+
 
 #             NOM DU           APPORT  PRIX   PRIX     CAPACITE
 #            TELEPHONE        INITIAL  /MOIS  DE BASE  DE STOCKAGE
-liste = {   'Iphone 14' :       [54.1, 44.9, 1019],     #128
-            'Iphone 13' :       [62.1, 36.9, 909],      #128
-            'Iphone 14 +':      [99,   49.9, 1299],     #256
-            'Iphone 14 Pro Max':[139,  64.9, 1479],     #128
-            'Iphone 14 Pro':    [129,  57.9, 1329],     #128
-            'Samsung S23' :     [99,   39.9, 959],      #256
-            'Samsung S23 Ultra':[129,  59.9, 1419],     #512
-            'Iphone 12' :       [79,   29.9, 809],      #64
-            'Samsung S22':      [-20,  32.9, 699],      #128
-            'Iphone 11' :       [69,   25.9, 529],      #64
-            'Samsung zFlip 4':  [129,  47.9, 594],      #128
-            'Samsung zFold 4':  [180,  79.9, 1089]      #256
+liste = {   'MARS  | Iphone 14' :       [54.1, 44.9, 1019],     #128
+            'MARS  | Iphone 13' :       [62.1, 36.9, 909],      #128
+            'MARS  | Iphone 14 +':      [99,   49.9, 1299],     #256
+            'MARS  | Iphone 14 Pro Max':[139,  64.9, 1479],     #128
+            'MARS  | Iphone 14 Pro':    [129,  57.9, 1329],     #128
+            '____  | Samsung S23' :     [99,   39.9, 959],      #256
+            '____  | Samsung S23 Ultra':[129,  59.9, 1419],     #512
+            'MARS  | Iphone 12' :       [79,   29.9, 809],      #64
+            '____  | Samsung S22':      [-20,  32.9, 699],      #128
+            '____  | Iphone 11' :       [69,   25.9, 529],      #64
+            '____  | Samsung zFlip 4':  [129,  47.9, 594],      #128
+            '____  | Samsung zFold 4':  [180,  79.9, 1089],     #256
+            
+            '____  | Iphone 14' :       [56.1, 42.9, 1019],     #128
+            '____  | Iphone 13' :       [59.1, 39.9, 1019],     #128
+            '____  | Iphone 14 Pro Max':[76.1, 62.9, 1019],     #128
+            '____  | Iphone 14 Pro':    [74.1, 54.9, 1329],     #128
+            '____  | Iphone 14 +':      [49.1, 49.9, 1299],     #256
+            '____  | Iphone 12' :       [47.1, 31.9, 809]       #64
    }
-
+# ____ = Prix actuel
 
 '''
 Fonction pour créer un fichier texte
 '''
 def txt(texte, n=0) :
-    nom = 'smartphones '+str(n)+'.txt'
     
-    if not os.path.isfile(nom) :
-        with open(nom, 'w') as fichier:
+    nom = 'smartphones '+str(n)+'.txt'
+    dir = os.path.join(os.path.dirname(__file__), nom)
+    
+    if not os.path.isfile(dir) :
+        with open(dir, 'w') as fichier:
             fichier.write(texte)
     
     else : txt(texte, n+1)
@@ -71,7 +80,7 @@ texte+='RENTABILITE :\n'
 classement = 1
 for i in rentabilite:
     
-    texte+=str(classement)+str((3-len(str(classement)))*' ')+str('  -   ')+str(i[0]+ (20-len(i[0]))*' '+f'({i[1]} mois)\n')
+    texte+=str(classement)+str((3-len(str(classement)))*' ')+str('  -   ')+str(i[0]+ (40-len(i[0]))*' '+f'({i[1]} mois)\n')
     classement += 1
    
     
@@ -83,7 +92,7 @@ texte+='\n\n\nPRIX SUR 1 AN :\n'
 classement = 1
 for i in prix_annuel:
     
-    texte+=str(classement)+str((3-len(str(classement)))*' ')+str('  -   ')+str(i[0])+str((20-len(i[0]))*' ')+str(f'({i[1]}€)\n')
+    texte+=str(classement)+str((3-len(str(classement)))*' ')+str('  -   ')+str(i[0])+str((40-len(i[0]))*' ')+str(f'({i[1]}€)\n')
     classement += 1
 
 
@@ -104,7 +113,7 @@ total = sorted(total.items(), key=lambda x:x[1])
 classement = 1
 for i in total:
     
-    texte+=str(classement)+str((3-len(str(classement)))*' ')+str('  -   ')+str(i[0])+str((20-len(i[0]))*' ')+str(f'(coût de {i[1]})\n')
+    texte+=str(classement)+str((3-len(str(classement)))*' ')+str('  -   ')+str(i[0])+str((40-len(i[0]))*' ')+str(f'(coût de {i[1]})\n')
     classement += 1
     
 texte+='\n\n\n\nMa page Github : https://github.com/bosy0'
